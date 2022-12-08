bb4bab053eddcddcb333ad827963f17f410fa05b
Ali Riza <115364922+AliRizaAynaci@users.noreply.github.com>
2022-12-08 23:08:40 +0300

    Time: 74 ms (44.66%), Space: 22.5 MB (48.42%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.cpp
+++ 0941-valid-mountain-array/0941-valid-mountain-array.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     bool validMountainArray(vector<int>& arr) {
-        if( arr.size() < 3 or arr[0]>=arr[1] or arr[arr.size()-1]>=arr[arr.size()-2])
+        
+        if(arr.size() < 3)
             return false;
-        bool increasing = true, decreasing = true;
-        int left = 0, right= arr.size()-1;
-        while(left<right and (increasing or decreasing)){
-			if(arr[left]<arr[left+1])    
-				left++;
-            else
-				// since it is not strictly increasing we will make it false
-                increasing = false;
-            if(arr[right]<arr[right-1])
-                right--;
-            else
-				// similarly if it not strictly decreasing we will make this false
-                decreasing = false;
-        }
-        return left==right;
+        int n = arr.size();
+        int i = 0;
+        //up
+        while(i < n-1 and arr[i] < arr[i+1])
+            i++;
+        
+        if(i == n-1 or i == 0)
+            return false;
+        
+        //down
+        while(i < n-1 and arr[i] > arr[i+1])
+            i++;
+        
+        return i == n-1;
     }
 };
