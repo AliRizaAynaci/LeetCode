a21c137698b5ac242a0e5278f1404769dbddbab1
Ali Riza <115364922+AliRizaAynaci@users.noreply.github.com>
2022-12-11 00:09:54 +0300

    Time: 0 ms (100.00%), Space: 6.2 MB (61.13%) - LeetHub

--- 1592-rearrange-spaces-between-words/1592-rearrange-spaces-between-words.cpp
+++ 1592-rearrange-spaces-between-words/1592-rearrange-spaces-between-words.cpp
@@ -1,26 +1,67 @@
 class Solution {
 public:
-    string reorderSpaces(string text) {
-        int spaces = 0;
-        string word, s;
-        vector<string> words;
-        for (auto ch : text) {
-            if (ch == ' ') {
-                ++spaces;
-                if (!word.empty())
-                    words.push_back(word);
-                word = "";
+	vector<string>words;
+    string reorderSpaces(string str) 
+	{
+        int space_count=0;
+        int idx = 0;
+        
+        while(idx < str.length())
+        {
+        	string temp="";
+        	while(idx<str.length() and (str[idx]>='a' and str[idx]<='z'))
+        	{
+        		temp.push_back(str[idx]);
+        		idx++;
+			}
+			while(idx<str.length() and str[idx] == ' ')
+			{
+				space_count++;
+				idx++;
+			}
+			
+			if(temp.length() > 0)
+			words.push_back(temp);
+		}
+        if(words.size()==1)
+        {
+            string s ="";
+            s.append(words[0]);
+
+            while(space_count > 0)
+            {
+                s.push_back(' ');
+                space_count--;
             }
-            else
-                word += ch;
+            return s;
+
         }
-        if (!word.empty())
-            words.push_back(word);
-        if (words.size() == 1)
-            return words.back() + string(spaces, ' ');
-        int gap = spaces / (words.size() - 1), suff = spaces % (words.size() - 1);
-        for (auto w : words)
-            s += w + string(gap, ' ');
-        return s.substr(0, s.size() - gap) + string(suff, ' ');
+        cout<<"space_count = "<<space_count<<endl;
+
+        if(space_count == 0) return str;
+
+		int used_space = space_count / (words.size()-1);
+		int extra_space = space_count % (words.size()-1);
+		
+		string ans = "";
+		for(int i=0;i<words.size();i++)
+		{
+			ans.append(words[i]);
+			
+			if(i == words.size()-1) break;
+			
+			int temp = used_space;
+			while(temp > 0)
+			{
+			    ans.push_back(' ');
+				temp--;	
+			}
+		}
+		while(extra_space > 0)
+		{
+			ans.push_back(' ');
+			extra_space--;
+		}
+		return ans;
     }
 };
