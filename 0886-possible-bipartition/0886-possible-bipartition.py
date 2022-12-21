683fc340b9b5297861c43406cabb500e75c6fb91
Ali Riza <115364922+AliRizaAynaci@users.noreply.github.com>
2022-12-21 11:37:09 +0300

    Time: 1471 ms (61.91%), Space: 20.1 MB (67.55%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.py
+++ 0886-possible-bipartition/0886-possible-bipartition.py
@@ -1,41 +1,35 @@
-class UnionFind:
-    def __init__(self, size):
-        self.parent = list(range(size))
-        self.rank = [0] * size
-    
-    def find(self, x):
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])
-        
-        return self.parent[x]
-    
-    def union_set(self, x, y):
-        xset = self.find(x)
-        yset = self.find(y)
-        if xset == yset:
-            return
-        
-        if self.rank[xset] < self.rank[yset]:
-            self.parent[xset] = yset
-        elif self.rank[xset] > self.rank[yset]:
-            self.parent[yset] = xset
-        else:
-            self.parent[yset] = xset
-            self.rank[xset] += 1
-        
 class Solution:
     def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
+        def bfs(source):
+            q = deque([source])
+            color[source] = 0
+            
+            while q:
+                node = q.popleft()
+                for neighbor in adj[node]:
+                    
+                    if color[neighbor] == color[node]:
+                        return False
+                    if color[neighbor] == -1:
+                        color[neighbor] = 1 - color[node]
+                        q.append(neighbor)
+            return True
+        
         adj = [[] for _ in range(n + 1)]
+        
         for dislike in dislikes:
             adj[dislike[0]].append(dislike[1])
             adj[dislike[1]].append(dislike[0])
-        
-        dsu = UnionFind(n + 1)
-        for node in range(1, n + 1):
-            for neighbor in adj[node]:
-                # Check if the node and its neighbor is in the same set.
-                if dsu.find(node) == dsu.find(neighbor): return False
-                # Move all the neighbours into same set as the first neighbour.
-                dsu.union_set(adj[node][0], neighbor)
+            
+        color = [-1] * (n + 1)
+        for i in range(1, n + 1):
+            if color[i] == -1:
+                
+                if not bfs(i):
+                    
+                    return False
         
         return True
+
+            
+            
