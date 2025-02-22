a5cad7dc96f18d72a8592ee2a87d6fdd46c3a1cb
Ali Rıza Aynacı <115364922+AliRizaAynaci@users.noreply.github.com>
2025-02-23 02:54:24 +0300

    Time: 25 ms (72.7%), Space: 23.8 MB (25.48%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,28 +1,46 @@
 class Solution {
-public:
-    int numIslands(vector<vector<char>>& grid) {
-        if (grid.size() == 0) return 0;
-        int island_counter = 0, m = grid.size(), n = grid[0].size();
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1') {
-                    dfs(grid, i, j, m, n);
-                    island_counter++;
+private:
+    void bfs(vector<vector<char>>& grid, int i, int j, int m, int n) {
+        queue<pair<int, int>> q;
+        q.push({i, j});
+        grid[i][j] = '0';
+        vector<pair<int, int>> directions = {{0, -1}, {0, 1}, {1, 0}, {-1, 0}};
+        while (!q.empty()) {
+            pair<int, int> p = q.front(); q.pop();
+            int k = p.first, l = p.second;
+            for (auto [dx, dy] : directions) {
+                int x = k + dx, y = l + dy;
+                if (x >= 0 && y >= 0 && x < m && y < n && grid[x][y] == '1') {
+                    q.push({x, y});
+                    grid[x][y] = '0';
                 }
             }
         }
-        return island_counter;
     }
 
-private:
     void dfs(vector<vector<char>>& grid, int i, int j, int m, int n) {
         // 1) out of bounds
-        // 2) grid[i][j] == 0
+        // 2) seen
         if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == '0') return;
         grid[i][j] = '0';
         dfs(grid, i, j - 1, m, n);
         dfs(grid, i, j + 1, m, n);
         dfs(grid, i - 1, j, m, n);
         dfs(grid, i + 1, j, m, n);
+    }
+
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        if (grid.size() == 0) return 0;
+        int island = 0, m = grid.size(), n = grid[0].size();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    bfs(grid, i, j, m, n);
+                    island++;
+                }
+            }
+        }
+        return island;
     }
 };
