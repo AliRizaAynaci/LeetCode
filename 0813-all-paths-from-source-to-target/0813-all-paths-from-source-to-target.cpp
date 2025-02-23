2056f9390411c05f2f6b5aaea67bac8485c7914e
Ali Rıza Aynacı <115364922+AliRizaAynaci@users.noreply.github.com>
2025-02-24 01:55:38 +0300

    Time: 0 ms (100%), Space: 13.7 MB (78.03%) - LeetHub

--- 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.cpp
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.cpp
@@ -1,28 +1,43 @@
 class Solution {
+private:
+    void dfs(vector<vector<int>>& graph, vector<vector<int>>& allPaths, vector<int>& path, int node) {
+        path.push_back(node);
+        if (node == graph.size() - 1) {
+            allPaths.push_back(path);
+        } else {
+            for (int neighbor : graph[node]) {
+                dfs(graph, allPaths, path, neighbor);
+            }
+        }
+        path.pop_back();
+    }
+
 public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
         vector<vector<int>> allPaths;
         if (graph.empty()) return allPaths;
-
-        int n = graph.size();
-        queue<vector<int>> q;
-        q.push({0});
-
-        while (!q.empty()) {
-            vector<int> path = q.front();
-            q.pop();
-
-            if (!path.empty() && path.back() == n - 1) {
-                allPaths.push_back(path);
-            } else {
-                int lastNode = path.back();
-                for (int neighbor : graph[lastNode]) {
-                    vector<int> newPath = path;
-                    newPath.push_back(neighbor);
-                    q.push(newPath);
-                }
-            }
-        }
+        vector<int> path;
+        dfs(graph, allPaths, path, 0);
         return allPaths;
+        // int n = graph.size();
+        // queue<vector<int>> q;
+        // q.push({0});
+
+        // while (!q.empty()) {
+        //     vector<int> path = q.front();
+        //     q.pop();
+
+        //     if (!path.empty() && path.back() == n - 1) {
+        //         allPaths.push_back(path);
+        //     } else {
+        //         int lastNode = path.back();
+        //         for (int neighbor : graph[lastNode]) {
+        //             vector<int> newPath = path;
+        //             newPath.push_back(neighbor);
+        //             q.push(newPath);
+        //         }
+        //     }
+        // }
+        // return allPaths;
     }
 };
