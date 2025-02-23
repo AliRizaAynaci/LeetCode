76926b8498234b8a329662e9d0c2cfc3eeda20a1
Ali Rıza Aynacı <115364922+AliRizaAynaci@users.noreply.github.com>
2025-02-23 03:06:22 +0300

    Time: 28 ms (51.22%), Space: 16 MB (95.98%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -56,7 +56,7 @@ public:
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '1') {
-                    bfs(grid, i, j, m, n);
+                    dfs(grid, i, j, m, n);
                     island++;
                 }
             }
