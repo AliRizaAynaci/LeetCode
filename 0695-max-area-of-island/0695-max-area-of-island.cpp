8661323451dd55ae32757ce911225f43619f86ba
Ali Rıza Aynacı <115364922+AliRizaAynaci@users.noreply.github.com>
2025-03-04 05:07:17 +0300

    Time: 0 ms (100%), Space: 27.5 MB (81.32%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,27 +1,28 @@
 class Solution {
 private:
     int getArea(vector<vector<int>>& grid, int i, int j, int m, int n) {
-        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == 0) return 0;
-        // if we come here, we know the current position is 1
-        grid[i][j] = 0;
+        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] < 1) {
+            return 0;
+        }
+        grid[i][j] = -1; // visited
         int left = getArea(grid, i, j - 1, m, n);
         int right = getArea(grid, i, j + 1, m, n);
-        int up = getArea(grid, i + 1, j, m, n);
-        int down = getArea(grid, i - 1, j, m, n);
-        return left + right + up + down + 1; 
+        int up = getArea(grid, i - 1, j, m, n);
+        int down = getArea(grid, i + 1, j, m, n);
+        return left + right + up + down + 1;
     }
     
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int maxSum = 0, m = grid.size(), n = grid[0].size();
+        if (grid.size() == 0) return 0;
+        int m = grid.size(), n = grid[0].size();
+        int maxArea= 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) {
-                    int area = getArea(grid, i, j, m, n);
-                    maxSum = max(maxSum, area);
-                }
+                int area = getArea(grid, i, j, m ,n);
+                maxArea = max(maxArea, area);
             }
         }
-        return maxSum;
+        return maxArea;
     }
 };
