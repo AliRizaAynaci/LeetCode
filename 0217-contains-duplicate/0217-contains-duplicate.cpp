2bfc9ae6a8fc442ba16f4bb88f552ab553935369
Ali Rıza Aynacı <115364922+AliRizaAynaci@users.noreply.github.com>
2025-06-19 01:24:29 +0300

    Time: 51 ms (57.84%), Space: 90.9 MB (46.38%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        unordered_set<int> set;
+
+        for (int i = 0; i < n; i++) {
+            if (set.find(nums[i]) != set.end()) {
+                return true;
+            }
+            set.insert(nums[i]);
+        }
+        return false;
+    }
+};
