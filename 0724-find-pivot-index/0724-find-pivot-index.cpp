9ec22cb4e6e036aae9ddc26083e856bb7b63145f
Ali Rıza Aynacı <115364922+AliRizaAynaci@users.noreply.github.com>
2025-08-08 17:30:12 +0300

    Time: 1 ms (29.12%), Space: 35.7 MB (77.12%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,22 +1,39 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> sumLeft(n, 0);
-        sumLeft[0] = nums[0];
-        for (int i = 1; i < n; i++) {
-            sumLeft[i] = sumLeft[i-1] + nums[i];
-        }
+        // Time Comp -> O(n)
+        // Space Comp -> O(1)
+        int sum = 0;
+        for (int num : nums) sum += num;
 
-        vector<int> sumRight(n, 0);
-        sumRight[n-1] = nums[n-1];
-        for (int i = n-2; i >= 0; i--) {
-            sumRight[i] = sumRight[i+1] + nums[i];
-        }
-        
-        for (int i = 0; i < n; i++) {
-            if (sumLeft[i] == sumRight[i]) return i;
+        int sumLeft = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (2*sumLeft + nums[i] == sum) return i;
+            sumLeft += nums[i];
         }
         return -1;
+        
+        // -----------------
+        
+        // Time Comp -> O(n)
+        // Space Comp -> O(n)
+        
+        // int n = nums.size();
+        // vector<int> sumLeft(n, 0);
+        // sumLeft[0] = nums[0];
+        // for (int i = 1; i < n; i++) {
+        //     sumLeft[i] = sumLeft[i-1] + nums[i];
+        // }
+
+        // vector<int> sumRight(n, 0);
+        // sumRight[n-1] = nums[n-1];
+        // for (int i = n-2; i >= 0; i--) {
+        //     sumRight[i] = sumRight[i+1] + nums[i];
+        // }
+        
+        // for (int i = 0; i < n; i++) {
+        //     if (sumLeft[i] == sumRight[i]) return i;
+        // }
+        // return -1;
     }
 };
