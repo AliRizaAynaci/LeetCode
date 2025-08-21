dba9dfc04b6a788d65676ba237ce1b2b236d3c08
Ali Rıza Aynacı <115364922+AliRizaAynaci@users.noreply.github.com>
2025-08-21 16:32:29 +0300

    Time: 0 ms (100%), Space: 71.8 MB (52.21%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        // Time Comp -> O(n)
+        // Space Comp -> O(1)
         if (nums.size() == 1) return nums[0];
         int currentSum = 0, maxSum = INT_MIN;
         for (int num : nums) {
