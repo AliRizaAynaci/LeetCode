72114ad1abd988b0de72a04dc103852820183bbf
Ali Rıza Aynacı <115364922+AliRizaAynaci@users.noreply.github.com>
2025-12-01 16:25:20 +0300

    Time: 259 ms (5.06%), Space: 149.3 MB (27.87%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
@@ -3,23 +3,23 @@ public:
     vector<vector<int>> prefix;
     NumMatrix(vector<vector<int>>& matrix) {
         int n = matrix.size(), m = matrix[0].size();
-        prefix.assign(n, vector<int>(m, 0));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                int up = (i > 0) ? prefix[i-1][j] : 0;
-                int left = (j > 0) ? prefix[i][j-1] : 0;
-                int diag = (i > 0 && j > 0) ? prefix[i-1][j-1] : 0;
-                prefix[i][j] = matrix[i][j] + up + left - diag;
+        prefix.assign(n+1, vector<int>(m+1, 0));
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                prefix[i][j] = matrix[i-1][j-1] 
+                            + prefix[i-1][j]
+                            + prefix[i][j-1]
+                            - prefix[i-1][j-1];
             }
         }
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
-        long long res = prefix[row2][col2];
-        if (row1 > 0) res -= prefix[row1-1][col2];
-        if (col1 > 0) res -= prefix[row2][col1-1];
-        if (row1 > 0 && col1 > 0) res += prefix[row1-1][col1-1];
-        return (int)res;
+        row1++; col1++; row2++; col2++;
+        return prefix[row2][col2] 
+            - prefix[row1-1][col2]
+            - prefix[row2][col1-1]
+            + prefix[row1-1][col1-1];
     }
 };
 
