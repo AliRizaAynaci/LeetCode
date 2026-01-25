a95579790ed5032d8eedc5d98a7758b461dd71d6
Ali Rıza Aynacı <115364922+AliRizaAynaci@users.noreply.github.com>
2026-01-26 00:50:30 +0300

    Time: 0 ms (100%), Space: 44.9 MB (50.34%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,14 +1,23 @@
 class Solution {
     public static int minCostClimbingStairs(int[] cost) {
-        int[] dp = new int[cost.length];
-        Arrays.fill(dp, -1);
-        return Integer.min(helper(dp, cost, 0), helper(dp, cost, 1));
+        int[] dp = new int[cost.length+1];
+        Arrays.fill(dp, 0);
+        for (int i = 2; i <= cost.length; i++) {
+            dp[i] = Integer.min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2]);
+        }
+        return dp[cost.length];
     }
 
-    public static int helper(int[] dp, int[] cost, int i) {
-        if (i >= cost.length) return 0;
-        if (dp[i] != -1) return dp[i];
-        dp[i] = cost[i] + Integer.min(helper(dp, cost, i+1), helper(dp, cost, i+2));
-        return dp[i];
-    }
+    // public static int minCostClimbingStairs(int[] cost) {
+    //     int[] dp = new int[cost.length];
+    //     Arrays.fill(dp, -1);
+    //     return Integer.min(helper(dp, cost, 0), helper(dp, cost, 1));
+    // }
+
+    // public static int helper(int[] dp, int[] cost, int i) {
+    //     if (i >= cost.length) return 0;
+    //     if (dp[i] != -1) return dp[i];
+    //     dp[i] = cost[i] + Integer.min(helper(dp, cost, i+1), helper(dp, cost, i+2));
+    //     return dp[i];
+    // }
 }
