faac7aac1ca2c246e48207fc038bd6a93bb2a273
Ali Rıza Aynacı <115364922+AliRizaAynaci@users.noreply.github.com>
2026-01-26 00:20:39 +0300

    Time: 0 ms (100%), Space: 42.3 MB (25.84%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,12 +1,27 @@
 class Solution {
-    public static int climbStairs(int n ) {
-        if (n <= 2) return n;
-        int[] dp = new int[n+1];
-        dp[1] = 1;
-        dp[2]= 2;
-        for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+
+    // Time Comp: O(N)
+    // Space Comp: O(1)
+    public static int climbStairs(int n) {
+        int first = 1, second = 1;
+        for (int i = 0; i < n-1; i++) {
+            int tmp = first;
+            first = first + second;
+            second = tmp;
         }
-        return dp[n];
+        return first;
     }
+
+    // Time Comp: O(n)
+    // Space Comp: O(n)
+    // public static int climbStairs(int n ) {
+    //     if (n <= 2) return n;
+    //     int[] dp = new int[n+1];
+    //     dp[1] = 1;
+    //     dp[2]= 2;
+    //     for (int i = 3; i <= n; i++) {
+    //         dp[i] = dp[i-1] + dp[i-2];
+    //     }
+    //     return dp[n];
+    // }
 }
