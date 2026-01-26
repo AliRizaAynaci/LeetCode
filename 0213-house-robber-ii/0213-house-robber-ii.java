cc19d25b285bda345bde09fc3c420926a1d8ed23
Ali Rıza Aynacı <115364922+AliRizaAynaci@users.noreply.github.com>
2026-01-26 03:23:59 +0300

    Time: 0 ms (100%), Space: 42.7 MB (70.51%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,22 +1,20 @@
 class Solution {
-    private static int helper(int[] nums) {
-        int n = nums.length;
-        if (n == 0) return 0;
-        if (n == 1) return nums[0];
-        int[] dp = new int[n];
-        dp[0] = nums[0];
-        dp[1] = Math.max(dp[0], nums[1]);
-        for (int i = 2; i < n; i++) {
-            dp[i] = Math.max(dp[i-1], nums[i] + dp[i-2]);
+    private static int helper(int[] nums, int start, int end) {
+        int len = end - start + 1;
+        if (len <= 0) return 0;
+        int[] dp = new int[len+1];
+        dp[0] = 0;
+        dp[1] = nums[start];
+        for (int i = 2; i <= len; i++) {
+            dp[i] = Math.max(dp[i-1], nums[start+i-1] + dp[i-2]);
         }
-        return dp[n-1];
+        return dp[len];
     }
 
     public static int rob(int[] nums) {
         int n = nums.length;
         if (n == 0) return 0;
         if (n == 1) return nums[0];
-        return Math.max(helper(Arrays.copyOfRange(nums, 0, nums.length - 1)),
-                helper(Arrays.copyOfRange(nums, 1, nums.length)));
+        return Math.max(helper(nums, 0, n-2), helper(nums, 1, n-1));
     }
 }
