ba6419a02246d5be5355ea66c695142fb36f1ac9
Ali Rıza Aynacı <115364922+AliRizaAynaci@users.noreply.github.com>
2026-01-26 04:24:43 +0300

    Time: 21 ms (38.51%), Space: 45.2 MB (46.4%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -34,15 +34,4 @@ class Solution {
         }
         return longestPalindrome;
     }
-
-    static{
-        Runtime.getRuntime().gc();
-        Runtime.getRuntime().addShutdownHook(new Thread(()->{
-            try(FileWriter f = new FileWriter("display_runtime.txt")){
-                f.write("0");
-            }catch(Exception e){
-
-            }
-        }));
-    }
 }
