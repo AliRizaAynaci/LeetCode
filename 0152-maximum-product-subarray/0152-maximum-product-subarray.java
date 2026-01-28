f38739d5d5f7a17ad3d990388eb6c10f87cbce9a
Ali Rıza Aynacı <115364922+AliRizaAynaci@users.noreply.github.com>
2026-01-28 22:20:45 +0300

    Time: 3 ms (19.45%), Space: 47.4 MB (24.95%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -3,17 +3,12 @@ class Solution {
         int n = nums.length;
         if (n == 0) return 0;
         if (n == 1) return nums[0];
-        int[][] dp = new int[n][2];
-        dp[0][0] = nums[0]; // 2, 6, -2, 4
-        dp[0][1] = nums[0]; // 2, 3, -12, -48
-        int maxProduct = nums[0];
-        // dp[i][0] = maxProduct, dp[i][1] = minProduct
-        for (int i = 1; i < n; i ++) {
-            // for max
-            dp[i][0] = Math.max(nums[i], Math.max(dp[i-1][0] * nums[i], dp[i-1][1] * nums[i]));
-            // for min
-            dp[i][1] = Math.min(nums[i], Math.min(dp[i-1][0] * nums[i], dp[i-1][1] * nums[i]));
-            maxProduct = Math.max(maxProduct, dp[i][0]);
+        int maxProduct = 0, currMax = 1, currMin = 1;
+        for (int i = 0; i < n; i++) {
+            int prevMax = currMax * nums[i];
+            currMax = Math.max(Math.max(currMax*nums[i], currMin*nums[i]), nums[i]);
+            currMin = Math.min(Math.min(currMin*nums[i], prevMax), nums[i]);
+            maxProduct = Math.max(maxProduct, currMax);
         }
         return maxProduct;
     }
