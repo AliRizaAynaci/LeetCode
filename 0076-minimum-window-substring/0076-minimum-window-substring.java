ff19f35b97ef5f89ad6526e70073d18c9ffd112f
Ali Rıza Aynacı <115364922+AliRizaAynaci@users.noreply.github.com>
2026-02-01 03:56:36 +0300

    Time: 21 ms (32.97%), Space: 47.8 MB (26.98%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,5 +1,5 @@
 class Solution {
-    public static String minWindow(String s, String t) {
+    public String minWindow(String s, String t) {
         if (s.length() < t.length()) return "";
         HashMap<Character, Integer> sMap = new HashMap<>();
         HashMap<Character, Integer> tMap = new HashMap<>();
