bd68afee83163ac4297f38d0b266e3d0967b3c87
Ali Rıza Aynacı <115364922+AliRizaAynaci@users.noreply.github.com>
2026-02-01 22:16:46 +0300

    Time: 29 ms (84.09%), Space: 150.1 MB (32.47%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -2,53 +2,34 @@ class Solution {
     // Time: O(n), Space: O(k)
     public static int[] maxSlidingWindow(int[] nums, int k) {
         int n = nums.length;
-        if (n < k) return new int[] {};
-        Deque<Integer> deque = new LinkedList<>();
-        int[] res = new int[n - k + 1];
-        int resIdx = 0;
-        for (int i = 0; i < n; i++) {
-            if (!deque.isEmpty() && deque.peekFirst() < i-k+1) {
-                deque.pollFirst();
+        if (n < k) {
+            return new int[] {};
+        }
+        int[] res = new int[n-k+1];
+        Deque<Integer> dq = new LinkedList<>();
+        int left = 0 , right = 0;
+        while (right < k) {
+            while (!dq.isEmpty() && dq.peekLast() < nums[right]) {
+                dq.pollLast();
             }
-            while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i]){
-                deque.pollLast();
+            dq.offerLast(nums[right]);
+            right++;
+        }
+        res[left] = dq.peekFirst();
+        left++;
+        while (right < n) {
+
+            int num = nums[right];
+            if (!dq.isEmpty() && nums[left-1] == dq.peekFirst()) {
+                dq.pollFirst();
             }
-            deque.offerLast(i);
-            if (i >= k - 1) {
-                res[resIdx] = nums[deque.peekFirst()];
-                resIdx++;
+            while (!dq.isEmpty() && dq.peekLast() < num) {
+                dq.pollLast();
             }
+            dq.offerLast(num);
+            res[left] = dq.peekFirst();
+            right++; left++;
         }
         return res;
-        // int n = nums.length;
-        // if (n < k) {
-        //     return new int[] {};
-        // }
-        // int[] res = new int[n-k+1];
-        // Deque<Integer> dq = new LinkedList<>();
-        // int left = 0 , right = 0;
-        // while (right < k) {
-        //     while (!dq.isEmpty() && dq.peekLast() < nums[right]) {
-        //         dq.pollLast();
-        //     }
-        //     dq.offerLast(nums[right]);
-        //     right++;
-        // }
-        // res[left] = dq.peekFirst();
-        // left++;
-        // while (right < n) {
-
-        //     int num = nums[right];
-        //     if (!dq.isEmpty() && nums[left-1] == dq.peekFirst()) {
-        //         dq.pollFirst();
-        //     }
-        //     while (!dq.isEmpty() && dq.peekLast() < num) {
-        //         dq.pollLast();
-        //     }
-        //     dq.offerLast(num);
-        //     res[left] = dq.peekFirst();
-        //     right++; left++;
-        // }
-        // return res;
     }
 }
