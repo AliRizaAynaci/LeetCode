daf1053afb1b7a350f362a750a5ee6f84286827f
Ali Rıza Aynacı <115364922+AliRizaAynaci@users.noreply.github.com>
2026-02-17 21:11:45 +0300

    Time: 7 ms (98.24%), Space: 45.4 MB (45.41%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,37 +1,32 @@
 class Solution {
-    private static int[] expand(String s, int mid) {
-        int l = mid-1, r = mid+1;
-        while (l > 0 && s.charAt(l) == s.charAt(mid)) {
-            l--;
-        }
-
-        while (r < s.length() && s.charAt(r) == s.charAt(mid)) {
-            r++;
-        }
-
-        while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+    private int[] expand(char[] str, int mid) {
+        int l = mid - 1, r = mid + 1;
+        
+        while (l > 0 && str[l] == str[mid]) l--;
+        while (r < str.length && str[r] == str[mid]) r++;
+        while (l >= 0 && r < str.length && str[l] == str[r]) {
             l--;
             r++;
         }
-
         return new int[] {l+1, r-1};
     }
 
-    public static String longestPalindrome(String s) {
+    public String longestPalindrome(String s) {
         if (s.length() <= 1) return s;
-        int n = s.length();
-        String longestPalindrome = "";
-        int longestLength = 0;
-        for (int i = 0; i < n; i++) {
-            int[] indexes = expand(s, i);
+        int longestLeft = 0, longestRight = 0;
+        int longestPalindrome = Integer.MIN_VALUE;
+        char[] str = s.toCharArray();
+        for (int i = 0; i < str.length; i++) {
+            int[] indexes = expand(str, i);
             int left = indexes[0];
             int right = indexes[1];
-            int currentLength = (right - left) + 1;
-            if (currentLength > longestLength) {
-                longestLength = currentLength;
-                longestPalindrome = s.substring(left, right+1);
+            int currLong = right - left + 1;
+            if (currLong >= longestPalindrome) {
+                longestPalindrome = currLong;
+                longestLeft = left;
+                longestRight = right;
             }
         }
-        return longestPalindrome;
+        return s.substring(longestLeft, longestRight+1);
     }
 }
