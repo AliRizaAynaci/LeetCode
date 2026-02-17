da3c7eac42850ddcf7e0b57c9c27bad5b2cac8a7
Ali Rıza Aynacı <115364922+AliRizaAynaci@users.noreply.github.com>
2026-02-17 15:54:50 +0300

    Time: 1 ms (85.16%), Space: 43.4 MB (39.28%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,14 +1,15 @@
 class Solution {
-    public static int ways(int[] dp, String s, int i) {
+    private static int[] dp;
+    public static int ways(String s, int i) {
         if (i == s.length()) return 1;
         if (s.charAt(i) == '0') return 0;
         if (dp[i] != -1) return dp[i];
-        dp[i] = ways(dp, s, i+1);
+        dp[i] = ways(s, i+1);
         if (i + 2 <= s.length()) {
             String even = s.substring(i, i+2);
             int num = Integer.parseInt(even);
             if (num >= 10 && num <= 26) {
-                dp[i] += ways(dp, s, i+2);
+                dp[i] += ways(s, i+2);
             }
         }
         return dp[i];
@@ -17,8 +18,8 @@ class Solution {
     public static int numDecodings(String s) {
         int n = s.length();
         // each idx in the dp array holds the count of different ways until the idx
-        int[] dp = new int[n];
+        dp = new int[n];
         Arrays.fill(dp, -1);
-        return ways(dp, s, 0);
+        return ways(s, 0);
     }
 }
