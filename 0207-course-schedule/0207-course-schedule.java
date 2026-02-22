37d3ad1eb5bf1934bb8e5ef611635821b85a8aad
Ali Rıza Aynacı <115364922+AliRizaAynaci@users.noreply.github.com>
2026-02-22 03:34:45 +0300

    Time: 10 ms (16.74%), Space: 47.3 MB (49.88%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        
         int n = prerequisites.length;
         if (n == 0) return true;
         int[] indegrees = new int[numCourses];
@@ -7,11 +8,11 @@ class Solution {
             int i = courses[0];
             indegrees[i]++;
         }
-        List<List<Integer>> adjList = new ArrayList<>();
-        for (int i = 0; i < numCourses; i++) {
-            adjList.add(new ArrayList<>());
-        }
+        HashMap<Integer, List<Integer>> adjList = new HashMap<>();
         for (int[] courses : prerequisites) {
+            if (!adjList.containsKey(courses[1])) {
+                adjList.put(courses[1], new ArrayList<>());
+            }
             adjList.get(courses[1]).add(courses[0]);
         }
         Queue<Integer> q = new LinkedList<>();
@@ -24,7 +25,8 @@ class Solution {
         while (!q.isEmpty()) {
             int course = q.poll();
             takenCount++;
-            for (int v : adjList.get(course)) {
+            List<Integer> neighbours = adjList.getOrDefault(course, new ArrayList<>());
+            for (int v : neighbours) {
                 indegrees[v]--;
                 if (indegrees[v] == 0) {
                     q.offer(v);
@@ -32,5 +34,38 @@ class Solution {
             }
         }
         return takenCount == numCourses;
+
+        // int n = prerequisites.length;
+        // if (n == 0) return true;
+        // int[] indegrees = new int[numCourses];
+        // for (int[] courses : prerequisites) {
+        //     int i = courses[0];
+        //     indegrees[i]++;
+        // }
+        // List<List<Integer>> adjList = new ArrayList<>();
+        // for (int i = 0; i < numCourses; i++) {
+        //     adjList.add(new ArrayList<>());
+        // }
+        // for (int[] courses : prerequisites) {
+        //     adjList.get(courses[1]).add(courses[0]);
+        // }
+        // Queue<Integer> q = new LinkedList<>();
+        // for (int i = 0; i < numCourses; i++) {
+        //     if (indegrees[i] == 0) {
+        //         q.offer(i);
+        //     }
+        // }
+        // int takenCount = 0;
+        // while (!q.isEmpty()) {
+        //     int course = q.poll();
+        //     takenCount++;
+        //     for (int v : adjList.get(course)) {
+        //         indegrees[v]--;
+        //         if (indegrees[v] == 0) {
+        //             q.offer(v);
+        //         }
+        //     }
+        // }
+        // return takenCount == numCourses;
     }
 }
