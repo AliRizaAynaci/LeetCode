e7fd851b1a69c553d6f8ebc4f3fd7cb80d190072
Ali Rıza Aynacı <115364922+AliRizaAynaci@users.noreply.github.com>
2026-02-26 09:51:44 +0300

    Time: 6 ms (12.79%), Space: 47.2 MB (7.7%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -1,31 +1,33 @@
 class Solution {
-    private boolean dfs(List<List<Integer>> adjList, boolean[] visited, int node, int prev) {
-        if (visited[node]) return true;
-        visited[node] = true;
-        for (int neighbour : adjList.get(node)) {
-            if (neighbour == prev) continue;
-            if (dfs(adjList, visited, neighbour, node)) {
-                return true;
-            }
+    private boolean dfsCycleDetection(List<List<Integer>> adj, boolean[] visited, int src, int parent) {
+        if (visited[src]) return true;
+        visited[src] = true;
+        for (int nei : adj.get(src)) {
+            if (nei == parent) continue;
+            if (dfsCycleDetection(adj, visited, nei, src)) return true;
         }
         return false;
     }
-    public int[] findRedundantConnection(int[][] edges) {
+
+    public int[] cycleDetection(int[][] edges) {
         int n = edges.length;
-        List<List<Integer>> adjList = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
-            adjList.add(new ArrayList<>());
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n+1; i++) {
+            adj.add(new ArrayList<>());
         }
         for (int[] edge : edges) {
-            int u = edge[0], v = edge[1];
-            adjList.get(u).add(v);
-            adjList.get(v).add(u);
-            boolean[] visit = new boolean[n + 1];
-
-            if (dfs(adjList, visit, u, -1)) {
-                return edge;
-            }
+            int src = edge[0], nei = edge[1];
+            adj.get(src).add(nei);
+            adj.get(nei).add(src);
+            boolean[] visited = new boolean[n+1];
+            if (dfsCycleDetection(adj, visited, src, -1)) return edge;
         }
-        return new int[] {};
+        return new int[0];
+    }
+
+
+
+    public int[] findRedundantConnection(int[][] edges) {
+        return cycleDetection(edges);
     }
 }
