d8241fa11aff32b66a98e59a5054867beb13443c
Ali Rıza Aynacı <115364922+AliRizaAynaci@users.noreply.github.com>
2026-02-26 10:08:11 +0300

    Time: 5 ms (17.1%), Space: 45.8 MB (20.92%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -1,26 +1,34 @@
 class Solution {
-    private boolean dfsCycleDetection(List<List<Integer>> adj, boolean[] visited, int src, int parent) {
-        if (visited[src]) return true;
-        visited[src] = true;
-        for (int nei : adj.get(src)) {
-            if (nei == parent) continue;
-            if (dfsCycleDetection(adj, visited, nei, src)) return true;
-        }
-        return false;
-    }
-
-    public int[] cycleDetection(int[][] edges) {
+    private int[] topologicalSort(int[][] edges) {
         int n = edges.length;
-        List<List<Integer>> adj = new ArrayList<>();
+        List<List<Integer>> adjList = new ArrayList<>();
+        int[] indegrees = new int[n + 1];
         for (int i = 0; i < n+1; i++) {
-            adj.add(new ArrayList<>());
+            adjList.add(new ArrayList<>());
         }
         for (int[] edge : edges) {
-            int src = edge[0], nei = edge[1];
-            adj.get(src).add(nei);
-            adj.get(nei).add(src);
-            boolean[] visited = new boolean[n+1];
-            if (dfsCycleDetection(adj, visited, src, -1)) return edge;
+            adjList.get(edge[0]).add(edge[1]);
+            adjList.get(edge[1]).add(edge[0]);
+            indegrees[edge[0]]++;
+            indegrees[edge[1]]++;
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            if (indegrees[i] == 1) q.offer(i);
+        }
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            indegrees[node]--;
+            for (int nei : adjList.get(node)) {
+                indegrees[nei]--;
+                if (indegrees[nei] == 1) q.offer(nei);
+            }
+        }
+        for (int i = n-1; i >= 0; i--) {
+            int src = edges[i][0], nei = edges[i][1];
+            if (indegrees[src] == 2 && indegrees[nei] > 0) {
+                return new int[] {src, nei};
+            }
         }
         return new int[0];
     }
@@ -28,6 +36,6 @@ class Solution {
 
 
     public int[] findRedundantConnection(int[][] edges) {
-        return cycleDetection(edges);
+        return topologicalSort(edges);
     }
 }
