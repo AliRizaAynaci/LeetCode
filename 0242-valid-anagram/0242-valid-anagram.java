e4cf2a9c84775e68cfff5e3150f5dba05a9f99fc
Ali Rıza Aynacı <115364922+AliRizaAynaci@users.noreply.github.com>
2026-05-21 15:14:07 +0300

    Time: 16 ms (21.91%), Space: 46.5 MB (32.51%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,33 +1,17 @@
 class Solution {
     public static boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) return false;
-        int[] arr = new int[26];
-        char[] sArr = s.toCharArray();
-        char[] tArr = t.toCharArray();
-        for (int i = 0; i < sArr.length; i++) {
-            arr[sArr[i] - 'a']++;
-            arr[tArr[i] - 'a']--;
+        HashMap<Character, Integer> freq = new HashMap<>();
+        for (char ch : s.toCharArray()) {
+            freq.put(ch, freq.getOrDefault(ch, 0) + 1);
         }
-
-        for (int val : arr) {
-            if (val != 0) return false;
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+            if (freq.containsKey(ch)) {
+                freq.put(ch, freq.get(ch) - 1);
+                if (freq.get(ch) == 0) freq.remove(ch);
+            }
         }
-        return true;
-//        if (s.length() != t.length()) return false;
-//        HashMap<Character, Integer> map = new HashMap<>();
-//        for (int i = 0; i < s.length(); i++) {
-//            char ch = s.charAt(i);
-//            map.put(ch, map.getOrDefault(ch, 0) + 1);
-//        }
-//        for (int i = 0; i < t.length(); i++) {
-//            char ch = t.charAt(i);
-//            if (!map.containsKey(ch)) {
-//                return false;
-//            }
-//            map.put(ch, map.get(ch)-1);
-//            if (map.get(ch) == 0) map.remove(ch);
-//        }
-//
-//        return true;
+        return freq.isEmpty();
     }
 }
