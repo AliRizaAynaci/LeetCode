f8013fe4ebf82124bdd9d85eb0b9d599a0630fce
Ali Rıza Aynacı <115364922+AliRizaAynaci@users.noreply.github.com>
2026-06-04 14:16:59 +0300

    0100-SAME-TREE Stats: Time: 0 ms (100%), Space: 43.1 MB (14.59%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -14,7 +14,7 @@
  * }
  */
 class Solution {
-    public static boolean isSameTree(TreeNode p, TreeNode q) {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null && q == null) return true;
         if (p == null || q == null || p.val != q.val) return false;
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
